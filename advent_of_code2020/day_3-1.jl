f63dd11b4249491f7e00d0ee6b971194bb4eca11
Alex Knudson <aknudson@nevada.unr.edu>
2020-12-03 20:19:25 -0800

    aoc day3

--- advent_of_code2020/day_3-1.jl
+++ advent_of_code2020/day_3-1.jl
@@ -1,27 +1,30 @@
-get_index(i, n) = ((i-1) % n) + 1
+using CircularArrays
+using Underscores
 
 
-struct HillMatrix <: AbstractArray{Int, 2}
-    terrain::BitMatrix
-end
+parse_terrain(str) = @_ readlines(str) .|>
+    collect .|> 
+    (_ .== '#') |>
+    hcat(__...) |> 
+    permutedims |>
+    CircularArray
 
-Base.size(H::HillMatrix) = size(H.terrain)
-Base.IndexStyle(::Type{<:HillMatrix}) = IndexCartesian()
 
-function Base.getindex(H::HillMatrix, i, j) 
-    # i = get_index(i, size(H.terrain, 1))
-    j = get_index(j, size(H.terrain, 2))
-    H.terrain[i,j]
-end
-
-function Base.show(io::IO, ::MIME"text/plain", H::HillMatrix)
-    println(io, "Rough terrain ahead!")
-    for r in eachrow(H)
-        println(io, join([i == 1 ? '#' : '.' for i in r]))
+function slide(H; right::Int, down::Int)
+    i, j = 1, 1
+    h, w = size(H)
+    s = 0
+    for _ in 1:down:h-1
+        j = j + right
+        i = i + down
+        s += H[i, j]
     end
+    s
 end
 
 
+h = parse_terrain("day3_input.txt")
+slide(h; right=3, down=1)
 
-h = HillMatrix(BitArray([1 0 0; 0 1 1]))
-h
+test_slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+prod([slide(h; right=r, down=d) for (r, d) in test_slopes])
