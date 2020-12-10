68f8333ecdb5b049e882f558210f0cab28ce6485
Alex Knudson <aknudson@nevada.unr.edu>
2020-12-10 10:51:27 -0800

    AoC day 8/9 finished

--- advent_of_code2020/day_8.jl
+++ advent_of_code2020/day_8.jl
@@ -1,29 +1,28 @@
-#= --- Day 8: Handheld Halting ---
-Your flight to the major airline hub reaches cruising altitude without incident. 
-While you consider checking the in-flight menu for one of those drinks that 
-come with a little umbrella, you are interrupted by the kid sitting next to you.
+### A Pluto.jl notebook ###
+# v0.12.16
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 25533c0e-3b06-11eb-3392-5dc41ce2cfeb
+md"""
+# Day 8: Handheld Halting
+
+Your flight to the major airline hub reaches cruising altitude without incident. While you consider checking the in-flight menu for one of those drinks that come with a little umbrella, you are interrupted by the kid sitting next to you.
 
 Their handheld game console won't turn on! They ask if you can take a look.
 
-You narrow the problem down to a strange infinite loop in the boot code 
-(your puzzle input) of the device. You should be able to fix it, but first you 
-need to be able to run the code in isolation.
+You narrow the problem down to a strange **infinite loop** in the boot code (your puzzle input) of the device. You should be able to fix it, but first you need to be able to run the code in isolation.
 
-The boot code is represented as a text file with one instruction per line of 
-text. Each instruction consists of an operation (acc, jmp, or nop) and an 
-argument (a signed number like +4 or -20).
+The boot code is represented as a text file with one **instruction** per line of text. Each instruction consists of an **operation** (`acc`, `jmp`, or `nop`) and an **argument** (a signed number like `+4` or `-20`).
 
-- acc increases or decreases a single global value called the accumulator by the 
-  value given in the argument. For example, acc +7 would increase the accumulator 
-  by 7. The accumulator starts at 0. After an acc instruction, the instruction 
-  immediately below it is executed next.
-- jmp jumps to a new instruction relative to itself. The next instruction to 
-  execute is found using the argument as an offset from the jmp instruction; for example, jmp +2 would skip the next instruction, jmp +1 would continue to the instruction immediately below it, and jmp -20 would cause the instruction 20 lines above to be executed next.
-- nop stands for No OPeration - it does nothing. The instruction immediately 
-  below it is executed next.
+- `acc` increases or decreases a single global value called the **accumulator** by the value given in the argument. For example, `acc +7` would increase the accumulator by 7. The accumulator starts at `0`. After an `acc` instruction, the instruction immediately below it is executed next.
+- `jmp` **jumps** to a new instruction relative to itself. The next instruction to execute is found using the argument as an **offset** from the `jmp` instruction; for example, `jmp +2` would skip the next instruction, `jmp +1` would continue to the instruction immediately below it, and `jmp -20` would cause the instruction `20` lines above to be executed next.
+- `nop` stands for **No OPeration** - it does nothing. The instruction immediately below it is executed next.
 
-  For example, consider the following program:
+For example, consider the following program:
 
+```
 nop +0
 acc +1
 jmp +4
@@ -33,9 +32,11 @@ acc -99
 acc +1
 jmp -4
 acc +6
+```
 
 These instructions are visited in this order:
 
+```
 nop +0  | 1
 acc +1  | 2, 8(!)
 jmp +4  | 3
@@ -45,46 +46,49 @@ acc -99 |
 acc +1  | 4
 jmp -4  | 5
 acc +6  |
+```
 
-First, the nop +0 does nothing. Then, the accumulator is increased from 0 to 1 
-(acc +1) and jmp +4 sets the next instruction to the other acc +1 near the
-bottom. After it increases the accumulator from 1 to 2, jmp -4 executes, 
-setting the next instruction to the only acc +3. It sets the accumulator to 5, 
-and jmp -3 causes the program to continue back at the first acc +1.
+First, the `nop +0` does nothing. Then, the accumulator is increased from 0 to 1 (`acc +1`) and `jmp +4` sets the next instruction to the other `acc +1` near the bottom. After it increases the accumulator from 1 to 2, `jmp -4` executes, setting the next instruction to the only `acc +3`. It sets the accumulator to 5, and `jmp -3` causes the program to continue back at the first `acc +1`.
 
-This is an infinite loop: with this sequence of jumps, the program will run 
-forever. The moment the program tries to run any instruction a second time, 
-you know it will never terminate.
+This is an **infinite loop**: with this sequence of jumps, the program will run forever. The moment the program tries to run any instruction a second time, you know it will never terminate.
 
-Immediately before the program would run an instruction a second time, the 
-value in the accumulator is 5.
+Immediately **before** the program would run an instruction a second time, the value in the accumulator is **`5`**.
 
-Run your copy of the boot code. Immediately before any instruction is executed 
-a second time, what value is in the accumulator?
-=#
+Run your copy of the boot code. Immediately before any instruction is executed a second time, **what value is in the accumulator?**
+"""
 
+# ╔═╡ cfe0deaa-3b0c-11eb-2ed3-1fb8b05d05e2
+parse_input(str) = [(Symbol(a), parse(Int, b)) for (a, b) in split.(readlines(str))]
+
+# ╔═╡ f81637da-3b07-11eb-187e-256189fd05f7
 mutable struct Instructions
-    pos::Int
-    acc::Int
-    num::Int
-    visited::Set{Int}
+	input::String
+	pos::Int
+	acc::Int
+	num::Int
+	visited::Set{Int}
     code::Array{Tuple{Symbol, Int64}, 1}
 
     Instructions(str) = begin
-        input = [(Symbol(a), parse(Int, b)) for (a, b) in split.(readlines(str))]
-        new(1, 0, length(input), Set{Int}(), input)
+        code = parse_input(str)
+        new(str, 1, 0, length(code), Set{Int}(), code)
     end
 
 end
 
+# ╔═╡ ff93cf2c-3b07-11eb-3207-c12a8135013d
 Base.push!(I::Instructions) = push!(I.visited, I.pos)
+
+# ╔═╡ 03e9c7f2-3b08-11eb-0381-25919343fe30
 Base.accumulate!(I::Instructions, v::Int) = I.acc += v
 
+# ╔═╡ 0813b7ca-3b08-11eb-20a8-0bc46c2d1edd
 function read_state!(I::Instructions)
     push!(I)
     I.code[I.pos]
 end
 
+# ╔═╡ 0c2bf02a-3b08-11eb-1923-257c1cdb6edf
 function execute_state!(I::Instructions, state)
     i, v = state
     if i == :nop
@@ -97,33 +101,38 @@ function execute_state!(I::Instructions, state)
     end
 end
 
+# ╔═╡ 1090cf16-3b08-11eb-0506-13c875038edc
 function run!(I::Instructions)
     while I.pos ∉ I.visited
         state = read_state!(I)
         execute_state!(I, state)
-        # I.pos > I.num && break
-    end
+		I.pos == I.num + 1 && return true
+	end
+	return false
 end
 
+# ╔═╡ 14926cc4-3b08-11eb-0f1f-8572570b8d18
+begin
+	boot_code = Instructions("day8_input.txt")
+	run!(boot_code)
+end
 
-# Part 1
-boot_code = Instructions("day8_input.txt")
-run!(boot_code)
-boot_code.acc
+# ╔═╡ 1ec7aeb8-3b08-11eb-21d0-237b6cbcae6e
+md"The accumulator value is: **$(boot_code.acc)**"
 
-#= Part 2
+# ╔═╡ b4f523da-3b07-11eb-354e-0b478aca87e4
+md"""
+## Part Two
 
-After some careful analysis, you believe that exactly one instruction is corrupted.
+After some careful analysis, you believe that **exactly one instruction is corrupted**.
 
-Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed
-to be a jmp. (No acc instructions were harmed in the corruption of this boot code.)
+Somewhere in the program, **either** a `jmp` is supposed to be a `nop`, or a `nop` is supposed to be a `jmp`. (No `acc` instructions were harmed in the corruption of this boot code.)
 
-The program is supposed to terminate by attempting to execute an instruction 
-immediately after the last instruction in the file. By changing exactly one jmp 
-or nop, you can repair the boot code and make it terminate correctly.
+The program is supposed to terminate by **attempting to execute an instruction immediately after the last instruction in the file**. By changing exactly one `jmp` or `nop`, you can repair the boot code and make it terminate correctly.
 
 For example, consider the same program from above:
 
+```
 nop +0
 acc +1
 jmp +4
@@ -133,15 +142,13 @@ acc -99
 acc +1
 jmp -4
 acc +6
+```
 
-If you change the first instruction from nop +0 to jmp +0, it would create a 
-single-instruction infinite loop, never leaving that instruction. If you change 
-almost any of the jmp instructions, the program will still eventually find 
-another jmp instruction and loop forever.
+If you change the first instruction from `nop +0` to `jmp +0`, it would create a single-instruction infinite loop, never leaving that instruction. If you change almost any of the `jmp` instructions, the program will still eventually find another `jmp` instruction and loop forever.
 
-However, if you change the second-to-last instruction (from jmp -4 to nop -4), 
-the program terminates! The instructions are visited in this order:
+However, if you change the second-to-last instruction (from `jmp -4` to `nop -4`), the program terminates! The instructions are visited in this order:
 
+```
 nop +0  | 1
 acc +1  | 2
 jmp +4  | 3
@@ -151,14 +158,59 @@ acc -99 |
 acc +1  | 4
 nop -4  | 5
 acc +6  | 6
+```
 
-After the last instruction (acc +6), the program terminates by attempting to 
-run the instruction below the last instruction in the file. With this change, 
-after the program terminates, the accumulator contains the 
-value 8 (acc +1, acc +1, acc +6).
+After the last instruction (`acc +6`), the program terminates by attempting to run the instruction below the last instruction in the file. With this change, after the program terminates, the accumulator contains the value 8 (`acc +1`, `acc +1`, `acc +6`).
 
-Fix the program so that it terminates normally by changing exactly one jmp 
-(to nop) or nop (to jmp). What is the value of the accumulator after the 
-program terminates?
+Fix the program so that it terminates normally by changing exactly one `jmp` (to `nop`) or `nop` (to `jmp`). **What is the value of the accumulator after the program terminates?**
+"""
 
-=#
+# ╔═╡ 9346dca8-3b0a-11eb-3e43-0ddcdf9d2e30
+function restart!(I::Instructions)
+	I.pos = 1;
+	I.acc = 0;
+	I.visited = Set{Int}();
+	I.code = parse_input(I.input);
+	return nothing
+end
+
+# ╔═╡ c974fc24-3b0a-11eb-0440-a9acb655243a
+function debug!(I::Instructions)
+	restart!(I)
+    for (i, c) in enumerate(I.code)
+		if c[1] == :acc
+			continue
+		elseif c[1] == :jmp
+			I.code[i] = (:nop, c[2])
+			run!(boot_code) && return (i, c)
+			restart!(boot_code)
+		else
+			I.code[i] = (:jmp, c[2])
+			run!(boot_code) && return (i, c)
+			restart!(boot_code)
+		end
+	end	
+end
+
+# ╔═╡ 352e7fe2-3b0d-11eb-3dc2-7b7e93a73373
+debug!(boot_code)
+
+# ╔═╡ 719a0834-3b0d-11eb-0cc6-4f4c95b2853b
+md"The accumulator value is: **$(boot_code.acc)**"
+
+# ╔═╡ Cell order:
+# ╟─25533c0e-3b06-11eb-3392-5dc41ce2cfeb
+# ╠═cfe0deaa-3b0c-11eb-2ed3-1fb8b05d05e2
+# ╠═f81637da-3b07-11eb-187e-256189fd05f7
+# ╠═ff93cf2c-3b07-11eb-3207-c12a8135013d
+# ╠═03e9c7f2-3b08-11eb-0381-25919343fe30
+# ╠═0813b7ca-3b08-11eb-20a8-0bc46c2d1edd
+# ╠═0c2bf02a-3b08-11eb-1923-257c1cdb6edf
+# ╠═1090cf16-3b08-11eb-0506-13c875038edc
+# ╠═14926cc4-3b08-11eb-0f1f-8572570b8d18
+# ╟─1ec7aeb8-3b08-11eb-21d0-237b6cbcae6e
+# ╟─b4f523da-3b07-11eb-354e-0b478aca87e4
+# ╠═9346dca8-3b0a-11eb-3e43-0ddcdf9d2e30
+# ╠═c974fc24-3b0a-11eb-0440-a9acb655243a
+# ╠═352e7fe2-3b0d-11eb-3dc2-7b7e93a73373
+# ╟─719a0834-3b0d-11eb-0cc6-4f4c95b2853b
--- advent_of_code2020/day_9.jl
+++ advent_of_code2020/day_9.jl
@@ -1,38 +1,40 @@
-#= --- Day 9: Encoding Error ---
-With your neighbor happily enjoying their video game, you turn your attention 
-to an open data port on the little screen in the seat in front of you.
+### A Pluto.jl notebook ###
+# v0.12.16
 
-Though the port is non-standard, you manage to connect it to your computer 
-through the clever use of several paperclips. Upon connection, the port outputs 
-a series of numbers (your puzzle input).
+using Markdown
+using InteractiveUtils
 
-The data appears to be encrypted with the eXchange-Masking Addition System 
-(XMAS) which, conveniently for you, is an old cypher with an important weakness.
+# ╔═╡ a5236852-3b11-11eb-27c2-8ba4ebe2cf26
+using OffsetArrays, IterTools
 
-XMAS starts by transmitting a preamble of 25 numbers. After that, each number 
-you receive should be the sum of any two of the 25 immediately previous numbers. 
-The two numbers will have different values, and there might be more than one such pair.
+# ╔═╡ 4a25fa2a-3b05-11eb-3dc5-218910f4128a
+md"""
+# Day 9: Encoding Error
 
-For example, suppose your preamble consists of the numbers 1 through 25 in a 
-random order. To be valid, the next number must be the sum of two of those numbers:
+With your neighbor happily enjoying their video game, you turn your attention to an open data port on the little screen in the seat in front of you.
 
-- 26 would be a valid next number, as it could be 1 plus 25 (or many other pairs, like 2 and 24).
-- 49 would be a valid next number, as it is the sum of 24 and 25.
-- 100 would not be valid; no two of the previous 25 numbers sum to 100.
-- 50 would also not be valid; although 25 appears in the previous 25 numbers, 
-  the two numbers in the pair must be different.
+Though the port is non-standard, you manage to connect it to your computer through the clever use of several paperclips. Upon connection, the port outputs a series of numbers (your puzzle input).
 
-Suppose the 26th number is 45, and the first number (no longer an option, as 
-it is more than 25 numbers ago) was 20. Now, for the next number to be valid, 
-there needs to be some pair of numbers among 1-19, 21-25, or 45 that add up to it:
+The data appears to be encrypted with the eXchange-Masking Addition System (XMAS) which, conveniently for you, is an old cypher with an important weakness.
 
-- 26 would still be a valid next number, as 1 and 25 are still within the previous 25 numbers.
-- 65 would not be valid, as no two of the available numbers sum to it.
-- 64 and 66 would both be valid, as they are the result of 19+45 and 21+45 respectively.
+XMAS starts by transmitting a **preamble** of 25 numbers. After that, each number you receive should be the sum of any two of the 25 immediately previous numbers. The two numbers will have different values, and there might be more than one such pair.
 
-Here is a larger example which only considers the previous 5 numbers 
-(and has a preamble of length 5):
+For example, suppose your preamble consists of the numbers `1` through `25` in a random order. To be valid, the next number must be the sum of two of those numbers:
 
+- `26` would be a **valid** next number, as it could be `1` plus `25` (or many other pairs, like `2` and `24`).
+- `49` would be a **valid** next number, as it is the sum of `24` and `25`.
+- `100` would **not** be valid; no two of the previous 25 numbers sum to `100`.
+- `50` would also **not** be valid; although `25` appears in the previous 25 numbers, the two numbers in the pair must be different.
+
+Suppose the 26th number is `45`, and the first number (no longer an option, as it is more than 25 numbers ago) was `20`. Now, for the next number to be valid, there needs to be some pair of numbers among `1-19`, `21-25`, or `45` that add up to it:
+
+- `26` would still be a **valid** next number, as `1` and `25` are still within the previous 25 numbers.
+- `65` would **not** be valid, as no two of the available numbers sum to it.
+- `64` and `66` would both be **valid**, as they are the result of `19+45` and `21+45` respectively.
+
+Here is a larger example which only considers the previous 5 numbers (and has a preamble of length 5):
+
+```
 35
 20
 15
@@ -53,12 +55,129 @@ Here is a larger example which only considers the previous 5 numbers
 277
 309
 576
+```
 
-In this example, after the 5-number preamble, almost every number is the sum of 
-two of the previous 5 numbers; the only number that does not follow this rule is 127.
+In this example, after the 5-number preamble, almost every number is the sum of two of the previous 5 numbers; the only number that does not follow this rule is `127`.
 
-The first step of attacking the weakness in the XMAS data is to find the first 
-number in the list (after the preamble) which is not the sum of two of the 25 
-numbers before it. What is the first number that does not have this property?
+The first step of attacking the weakness in the XMAS data is to find the first number in the list (after the preamble) which is not the sum of two of the 25 numbers before it. **What is the first number that does not have this property?**
 
-=#
+
+"""
+
+# ╔═╡ e0c8cf1c-3b10-11eb-2951-a53eecf0371c
+md"""
+## My Idea
+
+Use offset arrays to store the input with indices $[-d+1, -d+2, ..., 0, 1, ... n]$ where $n$ is the number of numbers *after* the preamble, and $d$ is the length of the preamble.
+
+Then I can reference the preceding numbers as $(A[i-d:i-1], A[i])$
+"""
+
+# ╔═╡ 006f8e7e-3b12-11eb-0273-bd69fc0bfd36
+struct XMAS
+	d::Int
+	n::Int
+	A::OffsetVector
+	XMAS(d, str) = begin
+		x = parse.(Int, readlines(str))
+		n = length(x) - d
+		new(d, n, OffsetVector(x, -d+1:n))
+	end
+end
+
+# ╔═╡ a3ec004e-3b12-11eb-1e78-516bc3ce7152
+Base.getindex(X::XMAS, i) = getindex(X.A, i)
+
+# ╔═╡ 9ae73e14-3b17-11eb-1448-cde659ccc526
+Base.eachindex(X::XMAS) = eachindex(X.A)
+
+# ╔═╡ 45b0c9e6-3b13-11eb-3d37-1d56f6f49772
+Base.size(X::XMAS) = size(X.A)
+
+# ╔═╡ 5a1c9cfc-3b13-11eb-0522-f189792ac4cb
+preamble(X::XMAS, i) = (X[i-X.d:i-1], X[i])
+
+# ╔═╡ 8e362bda-3b12-11eb-1b89-67b27ef785ce
+A = XMAS(25, "day9_input.txt");
+
+# ╔═╡ 134b3ada-3b14-11eb-2590-b7c2163a6d5b
+function debug(X::XMAS)
+	for i in 1:X.n
+		p, t = preamble(X, i)
+		t ∉ Set(sum(a) for a in subsets(p, Val{2}())) && return (index=i, value=t)
+	end
+end
+
+# ╔═╡ 0afdc21c-3b14-11eb-15b1-ed6b18356f5f
+debug(A)
+
+# ╔═╡ 67dfc938-3b14-11eb-107b-0d75d47b5570
+md"""
+## Part Two
+
+The final step in breaking the XMAS encryption relies on the invalid number you just found: you must **find a contiguous set of at least two numbers** in your list which sum to the invalid number from step 1.
+
+Again consider the above example:
+
+```
+35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576
+```
+
+In this list, adding up all of the numbers from 15 through 40 produces the invalid number from step 1, 127. (Of course, the contiguous set of numbers in your actual list might be much longer.)
+
+To find the **encryption weakness**, add together the **smallest** and **largest** number in this contiguous range; in this example, these are `15` and `47`, producing `62`.
+
+**What is the encryption weakness in your XMAS-encrypted list of numbers?**
+"""
+
+# ╔═╡ ea5997fa-3b15-11eb-1de0-13376ed9a412
+function hack(X::XMAS)
+	i, v = debug(X)
+	for n in 3:X.n
+		for s in partition(X.A, n, 1)
+			sum(s) == v && return s
+		end
+	end
+end
+
+# ╔═╡ 97bf08a8-3b16-11eb-2dc3-17ca57dd79b3
+ret = hack(A)
+
+# ╔═╡ 60c0559e-3b18-11eb-3337-8524e63ce9e3
+minimum(ret) + maximum(ret)
+
+# ╔═╡ Cell order:
+# ╟─4a25fa2a-3b05-11eb-3dc5-218910f4128a
+# ╟─e0c8cf1c-3b10-11eb-2951-a53eecf0371c
+# ╠═a5236852-3b11-11eb-27c2-8ba4ebe2cf26
+# ╠═006f8e7e-3b12-11eb-0273-bd69fc0bfd36
+# ╠═a3ec004e-3b12-11eb-1e78-516bc3ce7152
+# ╠═9ae73e14-3b17-11eb-1448-cde659ccc526
+# ╠═45b0c9e6-3b13-11eb-3d37-1d56f6f49772
+# ╠═5a1c9cfc-3b13-11eb-0522-f189792ac4cb
+# ╠═8e362bda-3b12-11eb-1b89-67b27ef785ce
+# ╠═134b3ada-3b14-11eb-2590-b7c2163a6d5b
+# ╠═0afdc21c-3b14-11eb-15b1-ed6b18356f5f
+# ╟─67dfc938-3b14-11eb-107b-0d75d47b5570
+# ╠═ea5997fa-3b15-11eb-1de0-13376ed9a412
+# ╠═97bf08a8-3b16-11eb-2dc3-17ca57dd79b3
+# ╠═60c0559e-3b18-11eb-3337-8524e63ce9e3
